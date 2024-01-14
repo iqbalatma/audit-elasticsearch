30092b5677f24beb6b3d4f28ae36a68626d062f9
iqbalatma <iqbalatma@gmail.com>
2024-01-14 13:17:06 +0700

    upgrading version for audit job

--- src/Jobs/AuditJob.php
+++ src/Jobs/AuditJob.php
@@ -15,7 +15,7 @@ class AuditJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public const PACKAGE_VERSION = "0.0.12";
+    public const PACKAGE_VERSION = "0.0.13";
 
     /**
      * Create a new job instance.
---
+++ src/Jobs/ReindexingJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Iqbalatma\AuditElasticsearch\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use JsonException;
+
+class ReindexingJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     * @throws JsonException
+     */
+    public function handle(): void
+    {
+
+    }
+}
